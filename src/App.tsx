b18a10db623cd489d2898ed30d258824630306ba
Inezaxelle <inezaaxelle44@gmail.com>
2023-02-24 10:28:48 +0200

    resolved conflicts

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,20 @@
-<<<<<<< HEAD
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Signup from "./components/signup/Signup";
 import Messaging from "./components/messaging/Messaging";
+import Login from "./components/Login/Login";
 ("./components/signup/Signup");
-=======
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Login from './components/Login/Login';
-import Signup from './components/signup/Signup'; './components/signup/Signup';
->>>>>>> origin/main
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
-<<<<<<< HEAD
     <BrowserRouter>
       <Routes>
         <Route path="signup" element={<Signup />}></Route>
         <Route path="chat" element={<Messaging />}></Route>
+        <Route path="login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
-=======
-
-    <BrowserRouter>
-      <Routes>
-        <Route path='signup' element={<Signup />} />
-        <Route path='login' element={<Login />} />
-      </Routes>
-    </BrowserRouter>
-
-  )
->>>>>>> origin/main
 }
 
 export default App;
