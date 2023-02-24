a3fd33fcee81f229cc46ed939804fe31ac78cdc2
Vanex-h <vanessahirwa5@gmail.com>
2023-02-24 07:07:09 +0200

    Login and Signup without responsiveness

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,18 @@
-import {BrowserRouter, Routes,Route} from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './components/Login/Login';
 import Signup from './components/signup/Signup'; './components/signup/Signup';
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
-    
-     <BrowserRouter>
-     <Routes>
-      <Route path='signup' element={<Signup />}> 
-    </Route>
-     </Routes>
-     </BrowserRouter>
-    
+
+    <BrowserRouter>
+      <Routes>
+        <Route path='signup' element={<Signup />} />
+        <Route path='login' element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+
   )
 }
 
---
+++ src/components/Login/LoginLeft.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Login.css'
+const LoginLeft=()=>{
+  return (
+    <div className='text-white lg:flex flex-col login-left w-[40%] justify-end p-6 hidden '>
+    <h2 className='md py-2'>Welcome back!</h2>
+    <span className='text-sm text-[rgba(255,255,255,0.66)] '>Login to continue spreading and <br />discovering new talents all over <br />the globe . </span>
+</div>
+  )
+}
+export default  LoginLeft
