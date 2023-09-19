a74e922798766a6529b98f577366465494a2833d
ibenitha <ibenitha03@gmail.com>
2023-09-19 19:27:47 +0200

    update(): fix issue

--- src/components/videocard/VideoCard.tsx
+++ src/components/videocard/VideoCard.tsx
@@ -17,21 +17,73 @@ interface VideoCardProps {
     comments: string[];
   };
 }
-
 const VideoCard = ({ video }: VideoCardProps) => {
   const [newComment, setNewComment] = useState("");
   const [comments, setComments] = useState(video.comments);
+  const [liked, setLiked] = useState(false);
+  const [disliked, setDisliked] = useState(false);
+  const [goldenBuzzerCount, setGoldenBuzzerCount] = useState(video.buzzers);
+  const [paymentModalOpen, setPaymentModalOpen] = useState(false);
 
   const handleLike = () => {
-    // Logic for handling the like action
+    // If already liked, remove the like
+    if (liked) {
+      setLiked(false);
+      // Decrease the like count
+      // You can also update your backend here
+      video.likes -= 1;
+    } else {
+      // If not liked, add the like
+      setLiked(true);
+      // Increase the like count
+      // You can also update your backend here
+      video.likes += 1;
+
+      // If already disliked, remove the dislike
+      if (disliked) {
+        setDisliked(false);
+        // Decrease the dislike count
+        // You can also update your backend here
+        video.dislikes -= 1;
+      }
+    }
   };
 
   const handleDislike = () => {
-    // Logic for handling the dislike action
+    // If already disliked, remove the dislike
+    if (disliked) {
+      setDisliked(false);
+      // Decrease the dislike count
+      // You can also update your backend here
+      video.dislikes -= 1;
+    } else {
+      // If not disliked, add the dislike
+      setDisliked(true);
+      // Increase the dislike count
+      // You can also update your backend here
+      video.dislikes += 1;
+
+      // If already liked, remove the like
+      if (liked) {
+        setLiked(false);
+        // Decrease the like count
+        // You can also update your backend here
+        video.likes -= 1;
+      }
+    }
   };
 
   const handleGoldenBuzzer = () => {
-    // Logic for handling the golden buzzer action
+    // Open the payment modal
+    setPaymentModalOpen(true);
+  };
+
+  const handlePayment = () => {
+    // Process the payment
+    // After successful payment, increment the golden buzzer count
+    setGoldenBuzzerCount(goldenBuzzerCount + 1);
+    // Close the payment modal
+    setPaymentModalOpen(false);
   };
 
   const handleCommentChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -64,40 +116,65 @@ const VideoCard = ({ video }: VideoCardProps) => {
      
         <div className="absolute bottom-0 left-0 w-full bg-gray-800 bg-opacity-90 rounded-b-none">
           <div className="flex items-center justify-center space-x-20">
-            {/* Likes */}
-            <div className="flex items-center">
-              <span className="text-gray-400 text-sm mr-1">{video.likes}</span>
+           {/* Likes */}
+          <div className="flex items-center">
+            <span className="text-gray-400 text-sm mr-1">{video.likes}</span>
+            <button
+              className={`text-xl hover:text-green-300 ${
+                liked ? "text-green-500" : "text-gray-500"
+              }`}
+              onClick={handleLike}
+            >
+              <FaThumbsUp />
+            </button>
+          </div>
+          {/* Dislikes */}
+          <div className="flex items-center">
+            <span className="text-gray-400 text-sm mr-1">{video.dislikes}</span>
+            <button
+              className={`text-xl hover:text-red-300 ${
+                disliked ? "text-red-500" : "text-gray-500"
+              }`}
+              onClick={handleDislike}
+            >
+              <FaThumbsDown />
+            </button>
+          </div>
+          {/* Golden Buzzers */}
+          <div className="flex items-center">
+            <span className="text-gray-400 text-sm mr-1">
+              {goldenBuzzerCount}
+            </span>
+            <button
+              className="text-xl text-yellow-400 hover:text-yellow-300"
+              onClick={handleGoldenBuzzer}
+            >
+              <FaStar />
+            </button>
+            </div>
+     {/* Golden Buzzer Payment Modal */}
+     {paymentModalOpen && (
+        <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-60">
+          <div className="bg-white p-4 rounded-md shadow-lg">
+            <h2 className="text-lg font-semibold">Pay to Send Golden Buzzer</h2>
+            <p className="mb-4 text-gray-600">Pay $10 to send a Golden Buzzer.</p>
+            <div className="flex justify-end">
               <button
-                className="text-xl text-green-500 hover:text-green-300"
-                onClick={handleLike}
+                className="px-3 py-1 text-xs bg-yellow-500 text-white rounded-sm hover:bg-yellow-600"
+                onClick={handlePayment}
               >
-                <FaThumbsUp />
+                Pay $10
+              </button>
+              <button
+                className="ml-2 px-3 py-1 text-xs text-gray-500 hover:text-gray-700"
+                onClick={() => setPaymentModalOpen(false)}
+              >
+                Cancel
               </button>
             </div>
-            {/* Dislikes */}
-            <div className="flex items-center">
-              <span className="text-gray-400 text-sm mr-1">
-                {video.dislikes}
-              </span>
-              <button
-                className="text-xl text-red-500 hover:text-red-300"
-                onClick={handleDislike}
-              >
-                <FaThumbsDown />
-              </button>
-            </div>
-            {/* Golden Buzzers */}
-            <div className="flex items-center">
-              <span className="text-gray-400 text-sm mr-1">
-                {video.buzzers}
-              </span>
-              <button
-                className="text-xl text-yellow-400 hover:text-yellow-300"
-                onClick={handleGoldenBuzzer}
-              >
-                <FaStar />
-              </button>
-            </div>
+          </div>
+        </div>
+      )}
           </div>
         </div>
       </div>
